7d86426c2083373e2a1e287ce0a61f211b28d952
Connor Wiseman <wiseman.connor@gmail.com>
2017-11-10 07:25:28 -0800

    test/unit/index.spec.js

--- test/unit/index.spec.js
+++ test/unit/index.spec.js
@@ -4,4 +4,52 @@
 'use strict';
 
 
-const prompt = require('../../lib');
+const chai           = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+const proxyquire     = require('proxyquire');
+const should         = chai.should();
+const sinon          = require('sinon');
+const sinonChai      = require('sinon-chai');
+
+
+chai.use(chaiAsPromised);
+chai.use(sinonChai);
+
+
+const promptStub       = sinon.stub().returns(Promise.resolve());
+const promptSilentStub = sinon.stub().returns(Promise.resolve());
+
+
+const prompt = proxyquire('../../lib', {
+  './prompt': promptStub,
+  './prompt-silent': promptSilentStub
+});
+
+
+describe('lib/index.js', () => {
+  it('should export `string` and `password` functions', () => {
+    prompt.should.have.all.keys('string', 'password');
+  });
+
+  describe('#string', () => {
+    it('should be a function', () => {
+      prompt.string.should.exist;
+      prompt.string.should.be.a('function');
+    });
+
+    it('should return a Promise', () => {
+      prompt.string('').should.be.a('Promise');
+    });
+  });
+
+  describe('#password', () => {
+    it('should be a function', () => {
+      prompt.password.should.exist;
+      prompt.password.should.be.a('function');
+    });
+
+    it('should return a Promise', () => {
+      prompt.password('').should.be.a('Promise');
+    });
+  });
+});
